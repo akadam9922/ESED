c725beaea809ca033bd5ea8873c36e531e237181
skadam62 <ccieshailendra@gmail.com>
2024-03-01 21:38:21 +0000

    created the springboot project

---
+++ demo/src/main/java/com/enterprise/software/demo/EsedCwApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.software.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsedCwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsedCwApplication.class, args);
+	}
+
+}
